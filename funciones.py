88075d2cdbeefa2329e4c6753286bde03aff9d9c
jonathanjavierloorduque <77116246+jonathanjavierloorduque@users.noreply.github.com>
2021-01-07 14:20:54 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,1 +1,32 @@
-print("hola")
+print("Funciones en Phyton")
+#reutilizacion de codigo  Don t Repeat yourself
+# sea más fácil de mantener DRY
+print("Hello word!")
+
+numeros=list(range(2, 20))
+print(numeros)
+str(12)
+range(10,20,3)
+#cuantos argumentos hay en esta función.
+numeros2=list(range(0,100,5))
+print(numeros2)
+print(len(numeros2))
+
+def mi_func():
+    print("spam")
+    print("spam")
+    print("spam")
+mi_func()
+
+
+def hello():
+    print("Hi!")
+hello():
+
+hello
+
+def hello():
+    print("hello word!")
+hello():
+
+
