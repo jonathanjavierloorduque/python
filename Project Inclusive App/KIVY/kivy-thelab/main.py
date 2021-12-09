261db4d3b1b9508f62b93a6b7cebebb596e4211d
jonathanjavierloorduque <77116246+jonathanjavierloorduque@users.noreply.github.com>
2021-12-09 15:01:00 -0800

    Reordenamiento
    Organización de proyectos

---
+++ Project Inclusive App/KIVY/kivy-thelab/main.py
@@ -0,0 +1,50 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
+
+
+# pip3 install kivy
+
+class StackLayoutExample(StackLayout):
+    def __init__(self,**kwargs):
+        super(). __init__(**kwargs)
+        for i in range (0,10):
+            b = Button(text=str(i+1), size_hint=(.2,.2))
+            self.add_widget(b)
+
+#class GridLayoutExample(GridLayout):
+#    pass
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+class BoxLayoutExample(BoxLayout):
+
+    pass
+
+
+
+"""
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        b3 = Button(text="C")
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
+"""
+class MainWidget(Widget):
+
+  pass
+
+class TheLabApp(App):
+    pass
+
+TheLabApp().run()
